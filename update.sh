0a3a3fd170e7b5e411d38d4c78acf25b108503a3
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-28 15:16:44 -0400

    Added documentation, git submodules

---
+++ update.sh
@@ -0,0 +1,12 @@
+# Updates NyWater.info git submodules
+cd nyw_data
+git pull origin master
+cd ..
+
+cd nyw_web_client
+git pull origin master
+cd ..
+
+cd nyw_web_api
+git pull origin master
+cd ..
